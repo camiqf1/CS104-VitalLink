cdf11f53c8ee9951e813d0a855f3526cc2d9f659
Camila Figueiredo <caq431@ensign.edu>
2026-04-07 10:09:28 -0600

    Completed Assignment

---
+++ sandbox/sandbox.py
@@ -0,0 +1,37 @@
+# Step 2
+name = "Aang"
+print(name)
+
+# Step 3
+name = "Katara"
+print(name)
+
+# Step 4
+name = "Aang" 
+title = "The Last Airbender"
+animal_companions = 2 
+age = 12 + 100 
+height = 165.5 
+is_bender = True 
+
+# Step 5
+print(type(name))
+print(type(title))
+print(type(animal_companions))
+print(type(age))
+print(type(height))
+print(type(is_bender))
+
+# Step 6a
+print(name, title, animal_companions, age, height, is_bender)
+print("Hello, my name is", name, "and I am", age, "years old.")
+
+# Step 6b
+print(name + title) 
+print(animal_companions + age + height) 
+print(name + str(age)) 
+
+print("Hello, my name is " + name + " and I am " + str(age) + " years old.")
+
+# Step 6c
+print("Variable:name Value:" + name, "Type:" + str(type(name)))
